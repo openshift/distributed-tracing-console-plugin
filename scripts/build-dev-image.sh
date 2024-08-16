32db09fd7751e81d10a8fc36e27bf5db64594f7a
PeterYurkovich <pyurkovi@redhat.com>
2024-08-16 10:32:04 -0400

    OU-446: Support FIPS

--- scripts/build-dev-image.sh
+++ scripts/build-dev-image.sh
@@ -1,9 +1,9 @@
-# Terminal output colors 
+# Terminal output colors
 GREEN='\033[0;32m'
 ENDCOLOR='\033[0m' # No Color
 RED='\033[0;31m'
 
-# Environment Variables 
+# Environment Variables
 PREFER_PODMAN="${PREFER_PODMAN:-0}"
 PUSH="${PUSH:-1}"
 TAG="${TAG:-dev}"
@@ -14,11 +14,11 @@ IMAGE=${BASE_IMAGE}:${TAG}
 printf "${GREEN}Environment Varibles ${ENDCOLOR}\n"
 printf "PREFER_PODMAN = ${PREFER_PODMAN}\n"
 printf "PUSH = ${PUSH}\n"
-printf "TAG = ${TAG}\n" 
+printf "TAG = ${TAG}\n"
 printf "REGISTRY_ORG = ${REGISTRY_ORG}\n"
 printf "IMAGE = ${IMAGE}\n"
 
-# User Input to confirm environment variables before proceeding. 
+# User Input to confirm environment variables before proceeding.
 read -p "$(printf "Do these env variables look right? (Y/N): ")" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 if [[ -x "$(command -v podman)" && $PREFER_PODMAN == 1 ]]; then
@@ -28,7 +28,7 @@ else
 fi
 
 echo "Building image '${IMAGE}' with ${OCI_BIN}"
-$OCI_BIN build -t $IMAGE --platform=linux/amd64 .
+$OCI_BIN build -t $IMAGE --platform=linux/amd64 -f Dockerfile.dev .
 
 if [[ $PUSH == 1 ]]; then
     echo "Pushing to registry with ${OCI_BIN}"
