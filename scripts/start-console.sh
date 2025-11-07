666ee10f120f615b6a2ce6a9e6f31788d245cf07
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-11-07 15:57:36 +0100

    NO-JIRA: various DX improvements
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- scripts/start-console.sh
+++ scripts/start-console.sh
@@ -6,6 +6,9 @@ CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:latest"}
 CONSOLE_PORT=${CONSOLE_PORT:=9000}
 CONSOLE_IMAGE_PLATFORM=${CONSOLE_IMAGE_PLATFORM:="linux/amd64"}
 
+# Plugin metadata is declared in package.json
+PLUGIN_NAME='distributed-tracing-console-plugin'
+
 echo "Starting local OpenShift console..."
 
 BRIDGE_USER_AUTH="disabled"
@@ -20,8 +23,7 @@ BRIDGE_K8S_MODE_OFF_CLUSTER_ALERTMANAGER=$(oc -n openshift-config-managed get co
 set -e
 BRIDGE_K8S_AUTH_BEARER_TOKEN=$(oc whoami --show-token 2>/dev/null)
 BRIDGE_USER_SETTINGS_LOCATION="localstorage"
-
-npm_package_consolePlugin_name='distributed-tracing-console-plugin'
+BRIDGE_I18N_NAMESPACES="plugin__${PLUGIN_NAME}"
 
 # Don't fail if the cluster doesn't have gitops.
 set +e
@@ -34,36 +36,37 @@ fi
 echo "API Server: $BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT"
 echo "Console Image: $CONSOLE_IMAGE"
 echo "Console URL: http://localhost:${CONSOLE_PORT}"
-echo "npm_package_consolePlugin_name : ${npm_package_consolePlugin_name}"
+echo "Console Platform: $CONSOLE_IMAGE_PLATFORM"
 
 # Prefer podman if installed. Otherwise, fall back to docker.
 if [ -x "$(command -v podman)" ]; then
     if [ "$(uname -s)" = "Linux" ]; then
         # Use host networking on Linux since host.containers.internal is unreachable in some environments.
-        BRIDGE_PLUGINS="${npm_package_consolePlugin_name}=http://localhost:9001"
-        podman run --pull always \
+        BRIDGE_PLUGINS="${PLUGIN_NAME}=http://localhost:9001"
+        podman run \
+        --pull always \
         --rm --network=host \
         --platform $CONSOLE_IMAGE_PLATFORM \
         --env-file <(set | grep BRIDGE) \
-        --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"http://localhost:9002","authorize":true}, {"consoleAPIPath": "/api/plugins/distributed-tracing-console-plugin/api/v1/list-tempostacks", "endpoint":"http://localhost:9002/api/v1/list-tempostacks","authorize":true}]}' \
+        --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"http://localhost:9002", "authorize": true}]}' \
         $CONSOLE_IMAGE
     else
-        BRIDGE_PLUGINS="${npm_package_consolePlugin_name}=http://host.containers.internal:9001"
+        BRIDGE_PLUGINS="${PLUGIN_NAME}=http://host.containers.internal:9001"
         podman run \
         --pull always \
         --platform $CONSOLE_IMAGE_PLATFORM \
         --rm -p "$CONSOLE_PORT":9000 \
         --env-file <(set | grep BRIDGE)  \
-        --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"http://host.containers.internal:9002","authorize":true}, {"consoleAPIPath": "/api/plugins/distributed-tracing-console-plugin/api/v1/list-tempostacks", "endpoint":"http://host.containers.internal:9002/api/v1/list-tempostacks","authorize":true}]}' \
+        --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"http://host.containers.internal:9002", "authorize": true}]}' \
         $CONSOLE_IMAGE
     fi
 else
-    BRIDGE_PLUGINS="${npm_package_consolePlugin_name}=http://host.docker.internal:9001"
+    BRIDGE_PLUGINS="${PLUGIN_NAME}=http://host.docker.internal:9001"
     docker run \
     --pull always \
     --platform $CONSOLE_IMAGE_PLATFORM \
     --rm -p "$CONSOLE_PORT":9000 \
     --env-file <(set | grep BRIDGE) \
-    --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"https://host.docker.internal:9002","authorize":true},{"consoleAPIPath": "/api/plugins/distributed-tracing-console-plugin/api/v1/list-tempostacks", "endpoint":"https://host.docker.internal:9002/api/v1/list-tempostacks","authorize":true}]}' \
+    --env BRIDGE_PLUGIN_PROXY='{"services": [{"consoleAPIPath": "/api/proxy/plugin/distributed-tracing-console-plugin/backend/", "endpoint":"https://host.docker.internal:9002", "authorize": true}]}' \
     $CONSOLE_IMAGE 
 fi
